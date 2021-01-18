cfe7752ee8dff45019d172bfeba8c93e6fe74be1
rafiq <rafiqul709@scuti.asia>
2021-01-18 10:19:54 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ *  You can use this class for importing Excel, words and text data.
+ */
+class importData
+{
+   /**
+    *  Store the interest of the client
+    * @var int $interest
+    */
+    private $interest;
+
+   /**
+    *  Calculate the interest of the client
+    *
+    * @param int $percent How many percentage of interest will be calculated
+    * @return int
+    */
+    public function calculate_interest($percent)
+    {
+        return $percent;
+    }
+
+}
+
